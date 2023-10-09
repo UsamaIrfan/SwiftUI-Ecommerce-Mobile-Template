5ec4df953fe4d22c8aec686efecfe9b2e0b84bbb
UsamaIrfan <usama.irfan9797@gmail.com>
2023-10-09 22:20:21 +0500

    Initial Commit

---
+++ mapapp/mapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapappApp.swift
+//  mapapp
+//
+//  Created by Map Mac on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct mapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
